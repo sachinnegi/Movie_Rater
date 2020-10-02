1ee69c26dfba90409698428e7ab31963a4da27b8
sachinnegi <negisachin688@gmail.com>
2020-10-02 22:49:25 +0530

    adding coursel

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import {movieURL} from '../../api';
 import CardArray from '../CardArray/CardArray';
+import CarouselAnimation from '../Carousel/CarouselAnimation'
 import './Home.css'
 function Home() {
     return (
         <div className="homepage">
+            <CarouselAnimation/>
             <CardArray heading = {"Popular Movies"} url={movieURL.popular} />
             <CardArray heading = {"Top Rated Movies"} url={movieURL.topRated} />
             <CardArray heading = {"Now Playing"} url={movieURL.now_playing} />
