062c791a48b2fe1c609ef6a2cade5a5a52a5ecc9
sachin negi <negisachin688@gmail.com>
2020-10-03 15:07:24 +0530

    added and modified carousel props

--- src/Components/Carousel/CarouselAnimation.js
+++ src/Components/Carousel/CarouselAnimation.js
@@ -6,7 +6,7 @@ function CarouselAnimation() {
     return (
         <div >
           
-            <Carousel autoplay speed= {1000} autoplaySpeed = {2500} className= 'car'>
+            <Carousel pauseOnHover ={false} autoplay speed= {1000} autoplaySpeed = {2400} className= 'car'>
                 <div className = "indiv">
                     <img className='image' src="http://image.tmdb.org/t/p/w342/riYInlsq2kf1AWoGm80JQW5dLKp.jpg"/>
                 </div>
