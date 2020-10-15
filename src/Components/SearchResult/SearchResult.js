1d3ef93885c1ccc87c87ba11ce15c771f26108c2
sachinnegi <negisachin688@gmail.com>
2020-10-15 22:40:40 +0530

    search container settings

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -1,31 +1,61 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import './SearchResult.css'
-import {key} from '../../api';
+import {key,img_base_url} from '../../api';
 import axios from 'axios';
-
-
-
-async function getSearchResult(query){
-    const searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`;
-    const response = await axios.get(searchUrl);
-    console.table(response)
-}
+import Loader from '../Loader/Loader'
 
 function SearchResult({searchInput}) {
-    
-    if (searchInput.length>0){
-        getSearchResult(searchInput)
+
+    const [movieArray, setMovieArray] = useState([])
+
+    async function getSearchResult(query){
+        try{
+            const searchUrl = `https://api.themoviedb.org/3/search/multi?api_key=${key}&language=en-US&query=${query}&page=1&include_adult=false`;
+            const response = await axios.get(searchUrl);
+            setMovieArray(response.data.results)
+            console.log(response.data.results);
+        }
+        catch(error){
+            console.log('Got error:-',error)
+        }
     }
+
     
-    return (
-        <div>
-            <p>'hi this is search page'</p>
-            <p>'hi this is search page'</p>
-            <p>'hi this is search page'</p>
-            <p>'hi this is search page'</p>
-            
-        </div>
-    )
+    
+    useEffect(() => {
+        if (searchInput.length>0){
+            getSearchResult(searchInput)
+        }
+    }, [searchInput])
+
+    
+    if (movieArray.length>0){
+        return (
+            <div className="search__card__container">
+                {
+                    movieArray.map( (item,idx) => ((
+                        item.poster_path && <div key={item.id} className="search__image__detailContainer">
+                            <img 
+                                className = "search__image" 
+                                alt='poster' 
+                                src={img_base_url+item.poster_path} 
+                            />
+                            <div className="search__detail">
+                                {item.name || item.title}
+                            </div>
+                        </div>
+                    )))
+                }
+                
+            </div>
+        )
+  }
+  else{
+      return(
+          <Loader />
+      )
+  }
+    
 }
 
 export default SearchResult
